6f23ad4351ba5fb0eee5c0f770d3fc3b25b3c4ef
Dennis Lefa Halo <72205474+LefaDev@users.noreply.github.com>
2022-04-02 03:26:59 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ 1. Connect to YouTube "Search" API
+ 2. Create an AJAX request to fetch data
+ 3. Render JSON data on the page with dynamic HTML
+ 4. Allow user to search (event listener) 
+*/
+
